85e68d066f0928dc50e24447740e1697b307c3c8
Randall Thomas <randallthomasmusic@gmail.com>
2022-01-07 08:09:56 -0800

    added scripts

---
+++ scripts/database.js
@@ -0,0 +1,12 @@
+const database = {
+    fish: [
+        {
+            name: "Bart",
+            food: "crustaceans",
+            size: 3
+        }
+    ]
+}
+export const getFish = () => {
+    return database.fish.map(fish => ({...fish}))
+}
